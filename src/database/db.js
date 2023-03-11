dd99a8caf216e285cfa2e92a7976f30f2f69a9b5
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-11 18:13:04 -0400

    Feat: adicionado informações sobr eo banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+const DB_NAME = "db_projeto_lavie";
+const DB_USER = "root";
+const DB_PASS = "12345678";
+const DB_CONFIGS = {
+  dialec: "mysql",
+  host: "localhost",
+  port: 3306,
+};
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIGS);
+
+(async () => {
+
+})
