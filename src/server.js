2167b3316b39af826311f482fffa70dbdc103e89
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-11 14:52:24 -0400

    Feat: criado esqueleto do crud dos pacientes

--- src/server.js
+++ src/server.js
@@ -2,8 +2,29 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "success" });
+const PORT = 3333;
+
+var pacientes = []
+
+app.get("/pacientes", (req, res) => {
+  return res.status(200).json({ pacientes });
 });
 
-app.listen(3333, () => onslotchange.log("server running in localhost:3333"));
+app.get("/pacients/:id", (req, res) => {
+  return res.status(200).json({ paciente });
+});
+
+app.post("/pacientes", (req, res) => {
+  return res.status(201).json({ paciente });
+});
+
+app.put("/pacientes/:id", (req, res) => {
+  return res.status(200).json({ paciente });
+});
+
+app.delete("/pacientes/:id", (req, res) => {
+  return res.status(204).send();
+});
+
+// Tem como função somente mostrar que o servidor está rodando
+app.listen(PORT, () => console.log(`server running in localhost:${PORT}`));
