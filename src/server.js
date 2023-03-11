b19fa62fe8567fa8d6ef09f69fd20ae8dc3ae345
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-11 14:36:28 -0400

    Feat: criada primeira rota get

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-import express from 'express';
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "success" });
+});
+
+app.listen(3333, () => onslotchange.log("server running in localhost:3333"));
