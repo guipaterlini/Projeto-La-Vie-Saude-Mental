b215105859cdecf04367322c60c2bb77fb140eca
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-11 15:04:21 -0400

    Feat: adiconado middleware para que expressa entenda json

--- src/server.js
+++ src/server.js
@@ -4,24 +4,36 @@ const app = express();
 
 const PORT = 3333;
 
-var pacientes = []
+// Middleware para que o express entenda json, precisa estar sempre acima das rotas
+app.use(express.json());
 
+var pacientes = [];
+
+// INSERT PACIENTES
+app.post("/pacientes", (req, res) => {
+  const { nome, email, idade } = req.body;
+
+  pacientes.push({ nome });
+
+  return res.status(201).json({ paciente });
+});
+
+// FIND ALL PACIENTES
 app.get("/pacientes", (req, res) => {
   return res.status(200).json({ pacientes });
 });
 
+// FIND ONE PACIENTE BY ID
 app.get("/pacients/:id", (req, res) => {
   return res.status(200).json({ paciente });
 });
 
-app.post("/pacientes", (req, res) => {
-  return res.status(201).json({ paciente });
-});
-
+// UPDATE PACIENTE BY ID
 app.put("/pacientes/:id", (req, res) => {
   return res.status(200).json({ paciente });
 });
 
+// DELETE PACIENTE BY ID
 app.delete("/pacientes/:id", (req, res) => {
   return res.status(204).send();
 });
