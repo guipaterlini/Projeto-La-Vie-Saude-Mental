119f379cc683368e7613fc75ac35234d0173af1f
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-12 18:11:46 -0400

    Fix: correção na validaçào de email para criar paciente

--- src/middlewares/verify-field-email.middleware.js
+++ src/middlewares/verify-field-email.middleware.js
@@ -11,14 +11,14 @@ export default function verifyFieldEmail(req, res, next) {
   }
 
   if (
-    usuario.length < 1 &&
-    dominio.length < 3 &&
-    usuario.search("@") != -1 &&
-    dominio.search("@") != -1 &&
-    usuario.search(" ") != -1 &&
-    dominio.search(" ") != -1 &&
-    dominio.search(".") == -1 &&
-    dominio.indexOf(".") < 1 &&
+    usuario.length < 1 ||
+    dominio.length < 3 ||
+    usuario.search("@") != -1 ||
+    dominio.search("@") != -1 ||
+    usuario.search(" ") != -1 ||
+    dominio.search(" ") != -1 ||
+    dominio.search(".") == -1 ||
+    dominio.indexOf(".") < 1 ||
     dominio.lastIndexOf(".") >= dominio.length - 1
   ) {
     return res.status(400).json({ message: ERROR_INVALID_EMAIL });
