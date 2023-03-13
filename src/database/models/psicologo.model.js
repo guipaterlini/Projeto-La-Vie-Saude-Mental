afd426ce50392a3a0a595cf1ae5ccb7b60f46291
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-12 23:11:06 -0400

    Feat: adicionado psicolog.model e seus dados

---
+++ src/database/models/psicologo.model.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import { db } from "../db.js";
+
+export const Psicologo = db.define(
+  "psicologo",
+  {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apresentacao: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "psicologo",
+  }
+);
