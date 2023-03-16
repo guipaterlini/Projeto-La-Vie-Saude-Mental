8ec458f3ca581015893601a700300acbead9c099
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-15 21:19:38 -0400

    adicinado o codigo do bruno nos crud de atendimento

---
+++ src/repositories/atendimentos/atendimentos.repository.js
@@ -0,0 +1,25 @@
+import { randomUUID } from "node:crypto";
+import { Atendimento } from "../../database/models/atendimento.model.js";
+
+export const createAtendimentoRepository = async (
+    paciente_id,  
+    psicologo_id,  
+    data_atendimento,  
+    observacao) => {    
+        return await Atendimento.create({     
+        id: randomUUID(),        
+        paciente_id,   
+        psicologo_id,   
+        data_atendimento,    
+        observacao, });
+};
+
+export const findAllAtendimentoRepository = async () => {
+    return await Atendimento.findAll();
+}
+
+export const findAtendimentoById = async (id) => {
+    const Atendimento = await Atendimento.findOne({ where: { id } });
+  
+    return Atendimento;
+}
