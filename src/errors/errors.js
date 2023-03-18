ea203e517d60e1efd6d2c52b11f31d98fef139d7
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-17 21:41:51 -0400

    Fix: corrigido o middleware de validação de email

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,16 +1,16 @@
-export const ERROR_TYPE_EMAIL = 'O campo "email" deve ser uma string';
+export const ERROR_TYPE_EMAIL = "O campo 'email' deve ser uma string";
 export const ERROR_INVALID_EMAIL =
   "O campo 'email' deve ter um email válido, sem espaços e com menos de 70 caracteres";
 export const ERROR_DUPLICATE_EMAIL = (email) => `O email ${email} já existe`;
 
 export const ERROR_SIZE_NAME =
   "O nome precisa conter mais que 3 letras e menos que 100 letras";
-export const ERROR_TYPE_NAME = 'O campo "nome" deve ser uma string';
+export const ERROR_TYPE_NAME = "O campo 'nome' deve ser uma string";
 
 export const ERROR_NOT_EXIST_BIO = "É preciso ter  um texto de 'apresentação'.";
 export const ERROR_SIZE_BIO =
   "A apresentação precisa ter menos de 700 caracteres";
-export const ERROR_TYPE_BIO = 'O campo "apresentação" deve ser uma string';
+export const ERROR_TYPE_BIO = "O campo 'apresentação' deve ser uma string";
 
 export const ERROR_INVALID_AGE =
   'O campo "idade" deve ser uma data válida no formato YYYY-MM-DD';
--- src/middlewares/verify-field-email.middleware.js
+++ src/middlewares/verify-field-email.middleware.js
@@ -3,13 +3,12 @@ import { ERROR_INVALID_EMAIL, ERROR_TYPE_EMAIL } from "../errors/errors.js";
 export default function verifyFieldEmail(req, res, next) {
   const { email } = req.body;
 
-  const usuario = email.substring(0, email.indexOf("@"));
-  const dominio = email.substring(email.indexOf("@") + 1, email.length);
-
   if (typeof email !== "string") {
     return res.status(400).json({ message: ERROR_TYPE_EMAIL });
   }
 
+  const usuario = email.substring(0, email.indexOf("@"));
+  const dominio = email.substring(email.indexOf("@") + 1, email.length);
   if (
     email.length > 70 ||
     usuario.length < 1 ||
